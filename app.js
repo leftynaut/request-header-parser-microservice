12565bea25f436e9aff35ac26f8e616d64fc19ab
leftynaut <mlefkowitz87@gmail.com>
2016-03-18 12:37:24 -0700

    all working. may update os name. html in place.

--- app.js
+++ app.js
@@ -2,32 +2,32 @@
 
 var express = require('express');
 
-var ip = require('ip');
-
+var ip = require('ip'); // module for getting ip address
 var os = require('os'); // built-in module for getting system info
 var osName = require('os-name'); // module for addtl system info
-
 var accepts = require('accepts'); // module for language header
 
 var app = express();
 
-app.use('/static', express.static(__dirname + '/public'));
-
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
-ip = ip.address(); // sets ip to ip variable
+ip = ip.address(); // sets ip address to ip var
+osName = osName(); // sets os name to osName var
 
-osName = osName(); // sets os name to os variable
-
-app.get('/', function(req, res) {
-    var path = req.path;
-    res.locals.path = path;
-    //res.render('index');
-    accepts = accepts(req).languages()[0]; // sets language to accepts variable
-    res.json({ ipaddress: ip, language: accepts, software: osName });
+app.get('/api', function(req, res) {
+    //var path = req.path;
+    //res.locals.path = path;
+    var lang = accepts(req).languages()[0]; // sets language to lang var
+    res.json({ ipaddress: ip, language: lang, software: osName });
 });
 
-app.listen(3000, function() { // change to process.env.PORT when on Heroku
+app.get('/*', function(req, res) { // wildcard catch-all
+    //var path = req.path;
+    //res.locals.path = path;
+    res.render('index');
+});
+
+app.listen(process.env.PORT, function() { // change to process.env.PORT when on Heroku
     console.log("request-header-parser-microservice running");
 });
