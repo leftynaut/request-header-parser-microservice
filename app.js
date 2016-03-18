7b5db0b37354113c0f42bdcf1a51eec451d8add6
leftynaut <mlefkowitz87@gmail.com>
2016-03-18 09:17:31 -0700

    using built-in module for ip

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var express = require('express')
+var express = require('express');
+var os = require('os');
 
 var app = express();
 
@@ -14,16 +15,19 @@ var ip_info;
 var get_ip = require('ipware')().get_ip;
 app.use(function(req, res, next) {
     ip_info = get_ip(req);
-    console.log(ip_info);
+    ip_info = ip_info.clientIp;
+    // console.log(ip_info);
     // { clientIp: '127.0.0.1', clientIpRoutable: false }
     next();
 });
+
+var ip = os.networkInterfaces().lo0[1].address;
 
 app.get('/', function(req, res) {
     var path = req.path;
     res.locals.path = path;
     //res.render('index');
-    res.json(ip_info);
+    res.json({ ipaddress: ip_info, software: ip });
 });
 
 app.listen(process.env.PORT, function() { // change to process.env.PORT when on Heroku
