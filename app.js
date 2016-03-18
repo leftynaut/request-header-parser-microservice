505df0d2d3fc9c6179507062a0252ccd5a7326d4
leftynaut <mlefkowitz87@gmail.com>
2016-03-18 11:43:02 -0700

    all working. may try to match OS name to example still.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var express = require('express');
-//var os = require('os');
+
+var ip = require('ip');
+
+var os = require('os'); // built-in module for getting system info
+var osName = require('os-name'); // module for addtl system info
+
 var accepts = require('accepts'); // module for language header
 
 var app = express();
@@ -11,27 +16,18 @@ app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
-var ip_info;
+ip = ip.address(); // sets ip to ip variable
 
-var get_ip = require('ipware')().get_ip;
-app.use(function(req, res, next) {
-    ip_info = get_ip(req);
-    ip_info = ip_info.clientIp;
-    // console.log(ip_info);
-    // { clientIp: '127.0.0.1', clientIpRoutable: false }
-    next();
-});
-
-//var ip = os.networkInterfaces().lo0[1].address;
+osName = osName(); // sets os name to os variable
 
 app.get('/', function(req, res) {
     var path = req.path;
     res.locals.path = path;
-    var lang = accepts(req).languages()[0];
     //res.render('index');
-    res.json({ ipaddress: ip_info, language: lang, software: null });
+    accepts = accepts(req).languages()[0]; // sets language to accepts variable
+    res.json({ ipaddress: ip, language: accepts, software: osName });
 });
 
-app.listen(process.env.PORT, function() { // change to process.env.PORT when on Heroku
+app.listen(3000, function() { // change to process.env.PORT when on Heroku
     console.log("request-header-parser-microservice running");
 });
