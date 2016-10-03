b32ee4d5c55c3337561abc04551ecf6f707e92d2
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-03 13:45:06 -0500

    fixed some minor bugs

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ var accepts = require('accepts'); // module for language header
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.get('/api', function(req, res) {
@@ -18,16 +17,21 @@ app.get('/api', function(req, res) {
     var os = req.headers['user-agent']; // requests user-agent headers
     os = uaparser.parseOS(os).toString(); // sets os string to var os
     var lang = accepts(req).languages()[0]; // sets language to var lang
-    res.json({ ipAddress: ip, language: lang, software: os });
+    res.json({
+        ipAddress: ip,
+        language: lang,
+        software: os
+    });
 });
 
 app.get('/*', function(req, res) { // wildcard catch-all
-    //var path = req.path;    --  not needed
-    //res.locals.path = path; --  not needed
-    res.render('index');
+    var local = req.get('host');
+    res.render('index', {
+        host: local
+    });
 });
 
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
-   console.log('Sever listening on port ' + port);
+    console.log('Sever listening on port ' + port);
 });
