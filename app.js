aff1c19b2b52b387a1c03a4f1ccaac521431fc77
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-04 13:51:42 -0500

    es6 updates

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 'use strict';
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var uaparser = require('ua-parser'); // module for user-agent parser
-var accepts = require('accepts'); // module for language header
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const uaparser = require('ua-parser'); // module for user-agent parser
+const accepts = require('accepts'); // module for language header
 
-var app = express();
+const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.get('/api', function(req, res) {
-    var ip = req.header('x-forwarded-for') || req.connection.remoteAddress; // sets ip to var ip
-    var os = req.headers['user-agent']; // requests user-agent headers
+
+app.get('/', (req, res) => {
+    let local = req.get('host');
+    res.render('index', {
+        host: local
+    });
+});
+
+app.get('/*', (req, res) => { // wildcard catch-all
+    let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+    let os = req.headers['user-agent']; // requests user-agent headers
     os = uaparser.parseOS(os).toString(); // sets os string to var os
-    var lang = accepts(req).languages()[0]; // sets language to var lang
+    let lang = accepts(req).languages()[0]; // sets language to var lang
     res.json({
         ipAddress: ip,
         language: lang,
@@ -24,14 +32,7 @@ app.get('/api', function(req, res) {
     });
 });
 
-app.get('/*', function(req, res) { // wildcard catch-all
-    var local = req.get('host');
-    res.render('index', {
-        host: local
-    });
-});
-
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function() {
-    console.log('Sever listening on port ' + port);
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function() {
+    console.log('Server listening on port ' + port);
 });
